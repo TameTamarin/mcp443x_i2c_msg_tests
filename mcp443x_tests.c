8edecf638c7c07ca5c8250db6277a29209716b5c
Tyler Rhoades <tylerrhoades13@gmail.com>
2024-11-03 12:22:13 -0600

    Added make file.  Added code to tests module to output a string stating the app has started.

---
+++ mcp443x_tests.c
@@ -0,0 +1,18 @@
+#include "Drivers/mcp443x_I2C_Msgs/Inc/mcp443x.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void num_to_bin_str(uint8_t num){
+    for(int i = 0; i<8; i++){
+        uint8_t bit_to_print = (num >> i) & 0b00000001;
+        printf("%d", bit_to_print);
+    }
+}
+
+void main(){
+    printf("starting mcp443x_tests");
+    num_to_bin_str(mcp44xx_address_byte(0));
+
+}
