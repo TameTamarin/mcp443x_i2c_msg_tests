62522a0005083d08dc914a9f11a5ea27f56a28fc
Tyler Rhoades <tylerrhoades13@gmail.com>
2024-11-08 23:41:51 -0600

    Fixed printing of start message and first test on the same line

--- mcp443x_tests.c
+++ mcp443x_tests.c
@@ -4,15 +4,15 @@
 #include <stdlib.h>
 
 
-// void num_to_bin_str(uint8_t num){
-//     for(int i = 0; i<8; i++){
-//         uint8_t bit_to_print = (num >> i) & 0b00000001;
-//         printf("%d", bit_to_print);
-//     }
-// }
+void print_num_to_bin_str(uint8_t num){
+    for(int i = 0; i<8; i++){
+        uint8_t bit_to_print = (num >> i) & 0b00000001;
+        printf("%d", bit_to_print);
+    }
+}
 
 int main(){
-    printf("starting mcp443x_tests");
-    //num_to_bin_str(mcp44xx_address_byte(0));
+    printf("starting mcp443x_tests\n");
+    print_num_to_bin_str(mcp44xx_address_byte(0));
     return 0;
 }
